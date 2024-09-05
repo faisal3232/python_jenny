6470762a0b6e162ff918aeb6feb37eb1d2395fd4
Faisal Shaikh <faisal.shaikh32@gmail.com>
2024-09-05 12:16:49 +0530

    created 1st python to print and README.md

---
+++ 1.py
@@ -0,0 +1,13 @@
+# first python program to print
+
+print("first program - python print function")
+print("It is declared like this :")
+print("print('what to print')" )
+
+print()
+
+print("first program - python print function\nIt is declared like this :\nprint('what to print')")
+
+print("faisal"+" "+"shaikh")
+
+print('print("hello"+"faisal")')
